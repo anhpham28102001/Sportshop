fd3ffb941a8fcf7283d4fa841b708730a18b6885
Phan Anh <phananh@MacBook-Pro-cua-Phan.local>
2023-10-08 18:32:31 +0700

    update buoi 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import { useState } from "react";
 import Demo from "./components/Admin/Demo/Demo";
-
+import DemoUser from "./components/Site/DemoUser/DemoUser";
 
 function App() {
-  return (
-    <>
-     <Demo></Demo>
-    </>
-  );
+  const [loginAdmin, setLoginAdmin] = useState(false);
+  return <DemoUser setLoginAdmin={setLoginAdmin}></DemoUser>;
 }
 
 export default App;
